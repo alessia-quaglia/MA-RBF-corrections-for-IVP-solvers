a4f8321d89a68202b4672cc71a80cce9a0ff720d
alessia-quaglia <alessia.quaglia929@edu.unito.it>
2026-09-01 10:25:03 +0200

    Update ab2am1.m

--- Classical methods for IVP/ab2am1.m
+++ Classical methods for IVP/ab2am1.m
@@ -1,19 +1,3 @@
-% function err = ab2am1(f, u_es, a, b, u0, N)
-%     h = (b-a)/N;
-%     t = linspace(a,b,N+1)';
-%     u = zeros(1, N+1); 
-%     u(1) = u0;
-%     u(2) = u_es(t(2));
-%     for n = 2:N
-%         fn = f(t(n),u(n));
-%         fn_1 = f(t(n-1),u(n-1));
-%         u_p = u(n) + h * (3/2 * fn - 1/2 * fn_1); % PREDITTORE (Adams-Bashforth 2) 
-%         t0 = f(t(n+1),u_p);
-%         u(n+1) = u(n) + (h/2) * (t0 + fn); % CORRETTORE (Adams-Moulton 1)
-%     end
-%     err = abs(u(end) - u_es(b));
-% end
-
 function err = ab2am1(f, u_es, a, b, u0, N)
 %
 % Usage:    err = ab2am1(f, u_es, a, b, u0, N)
@@ -29,17 +13,17 @@ function err = ab2am1(f, u_es, a, b, u0, N)
 %           N = controls the step size h
 % Output: err = global error at the final point t = b
 %
-h = (b-a)/N;
-t = linspace(a,b,N+1)';
+h = (b-a) / N;
+t = linspace(a, b, N+1)';
 u = zeros(N+1, 1); 
 u(1) = u0;
 u(2) = u_es(t(2));
 t2 = f(t(1),u(1));
 for n = 2:N
     t1 = f(t(n),u(n));
-    u_p = u(n) + h/2 * (3 * t1 - t2); % PREDITTORE (Adams-Bashforth 2) 
+    u_p = u(n) + h/2 * (3 * t1 - t2); 
     t0 = f(t(n+1),u_p);
-    u(n+1) = u(n) + (h/2) * (t0 + t1); % CORRETTORE (Adams-Moulton 1)
+    u(n+1) = u(n) + (h/2) * (t0 + t1); 
     if n < N
        t2 = t1;
     else
