5512cda5bdbc044791c0cad869c93ed04a132652
alessia-quaglia <alessia.quaglia929@edu.unito.it>
2026-09-01 10:23:35 +0200

    Update ab2.m

--- Classical methods for IVP/ab2.m
+++ Classical methods for IVP/ab2.m
@@ -1,18 +1,3 @@
-% function err = ab2(f, u_es, a, b, u0, N)
-%     h = (b-a)/N;
-%     t = linspace(a,b,N+1)';
-%     u = zeros(1, N+1); 
-%     u(1) = u0;
-%     u(2) = u_es(t(2));
-%     %u(2) = rk4_step(f, u(1), h);
-%     for n = 2:N
-%         fn = f(t(n),u(n)); 
-%         fn_1 = f(t(n-1),u(n-1));
-%         u(n+1) = u(n) + h*(3/2*fn - 1/2*fn_1);
-%     end
-%     err = abs(u(end) - u_es(b));
-% end
-
 function err = ab2(f, u_es, a, b, u0, N)
 %
 % Usage:    err = ab2(f, u_es, a, b, u0, N)
@@ -27,7 +12,7 @@ function err = ab2(f, u_es, a, b, u0, N)
 %           N = controls the step size h
 % Output: err = global error at the final point t = b
 %
-h = (b - a) / N;
+h = (b-a) / N;
 t = linspace(a, b, N+1)';
 u = zeros(N+1, 1);
 u(1) = u0;
